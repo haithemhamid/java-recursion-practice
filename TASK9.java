30a6f2036442bcbd04e2bb9afb7f591bb1d0e249
Haitham Alhinai <149400034+haithemhamid@users.noreply.github.com>
2025-09-09 17:31:00 +0400

    Update and rename TRA5302.java to TASK9.java

--- TASK9.java
+++ TASK9.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 import java.util.List;
-public class TRA5302 {
+public class TASK9 {
 
         public static void main(String[] args) {
             List<String> fruits = Arrays.asList("Apple", "Banana", "Orange", "Grape", "Mango");
