e4d914497aae16f3cbb933bafe6e0b7ad4a928ff
haithemhamid <haithemalhinai2017@gmail.com>
2025-09-09 17:05:18 +0400

    Solve task 6 in recursion practice

---
+++ TRA5280.java
@@ -0,0 +1,17 @@
+public class TRA5280 {
+    public static void main(String[] args) {
+        int count = 10;
+        System.out.println("Generating the first " + count + " Fibonacci numbers iteratively...");
+        // The main method will then be changed to:
+         for (int i = 0; i < count; i++) {
+             System.out.print(fibonacciRecursive(i) + " ");
+         }
+        System.out.println();
+    }
+    // TODO: Create a public static int fibonacciRecursive(int n) function here.
+    public static int fibonacciRecursive(int n){
+        if(n<=1)return 1;
+        return fibonacciRecursive(n-2)+fibonacciRecursive(n-1);
+    }
+
+}
