82d7393a1d23908ec5a8983128fbd0b6420a45a0
haithemhamid <haithemalhinai2017@gmail.com>
2025-09-09 16:55:58 +0400

    Solve task 5 in recursion practice

---
+++ TRA5273.java
@@ -0,0 +1,15 @@
+public class TRA5273 {
+    public static void main(String[] args) {
+        int base = 3;
+        int exponent = 4;
+        long result = 1;
+        System.out.println("Calculating " + base + "^" + exponent + " using a loop...");
+        result=powerRecursive(base,exponent);
+        System.out.println("Result: " + result); // Expected: 81
+    }
+    // TODO: Create a public static long powerRecursive(int base, int exp) function here.
+    public static long powerRecursive(int base, int exp){
+        if (exp==0)return 1;
+        return base*powerRecursive(base,exp-1);
+    }
+}
