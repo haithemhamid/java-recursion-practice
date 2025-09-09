9b88d91c88daf74a883820013a5dcc0d55195048
haithemhamid <haithemalhinai2017@gmail.com>
2025-09-09 16:30:51 +0400

    Solve task 2 in recursion practice

---
+++ TRA5235.java
@@ -0,0 +1,14 @@
+public class TRA5235 {
+    public static void main(String[] args) {
+        String original = "Hello World";
+        String reversed=reverseStringRecursive(original);
+        System.out.println("Original: " + original);
+        System.out.println("Reversed: " + reversed);
+    }
+
+    // TODO: Create a public static String reverseStringRecursive(String str) function here.
+    public static String reverseStringRecursive(String str){
+        if(str.isEmpty()) return "";
+        return str.charAt(str.length()-1)+reverseStringRecursive(str.substring(0,str.length()-1));
+    }
+}
