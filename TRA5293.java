48980b25c4716f0aa5bf7bfb4cc4f791e90f45de
haithemhamid <haithemalhinai2017@gmail.com>
2025-09-09 17:14:32 +0400

    Solve task 8 in recursion practice

---
+++ TRA5293.java
@@ -0,0 +1,15 @@
+public class TRA5293 {
+    public static void main(String[] args) {
+        int number = 12345;
+        int sum = 0;
+        System.out.println("Calculating the sum of digits for " + number + " with a loop...");
+        sum=sumDigitsRecursive(number);
+        System.out.println("The sum of the digits is: " + sum);
+    }
+    // TODO: Create a public static int sumDigitsRecursive(int number) function here.
+    public static int sumDigitsRecursive(int number){
+        if(number==0)return 0;
+        return number%10+sumDigitsRecursive(number/10);
+    }
+
+}
