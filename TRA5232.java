6567b26436438eff34aeafda9e28cffc86af4f90
haithemhamid <haithemalhinai2017@gmail.com>
2025-09-09 16:02:11 +0400

    Solve task 1 in recursion practice

---
+++ TRA5232.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class TRA5232 {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(10, 20, 30, 40, 50);
+        int totalSum=calculateSumRecursive( numbers, 0);
+        System.out.println("--------------------------");
+        System.out.println("The final sum is: " + totalSum);
+    }
+    public static int calculateSumRecursive(List<Integer> nums, int index){
+
+    // TODO: Create a public static int calculateSumRecursive(List<Integer> nums, int index) function here.
+        if(nums.size()==index) return 0;
+        return nums.get(index)+calculateSumRecursive(nums,index+1);
+}
+}
